1881a23eb505baeacd0a2b4605909957fbc4d6a9
najeebabdi <najeebwarsame@gmail.com>
2025-06-17 16:22:49 +0200

    promises update

---
+++ Promises.js
@@ -0,0 +1,65 @@
+const posts = [
+
+    {title: "Post One", body: "This is post one"},
+    { title: "Post Two", body: "This is post two" }  
+];
+
+const getPosts = () => {
+
+    // Takes in a callback function and a certain amount of time that delays whatever you put in the callback function
+    setTimeout(() => {
+
+    let output = "";
+    posts.forEach((posts, index) => {
+        output += `<li>${posts.title}</li>`;
+    });
+    document.body.innerHTML = output;
+    }, 1000);
+
+}
+
+const createPost = (post) => {
+
+    // Promise parameters - when you want to resolve a promise successfully you call resolve and when something goes wrong and there
+    // is some kind of reject of error you call reject
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            posts.push(post);
+
+            const error = false;
+
+            if(!error) {
+              resolve();
+            } else {
+                reject("Error: Something went wrong");
+            }
+       
+       
+        }, 2000);
+
+    });
+}
+    /*
+    createPost({title: "Post Three", body: "This is post three"})   
+    .then(getPosts)
+    .catch(err => console.log(err));
+    */
+
+    /*
+
+    const promise1 = Promise.resolve("Hello World");
+    const promise2 = 10;
+    const promise3 = new Promise((resolve, reject) => 
+        
+        setTimeout(resolve, 2000, "Goodbye"));
+
+        // NB: FETCH is a bit weird, it requires two .then methods because you firstly have to format the data
+        // into JSON and then retrieve it
+        const promise4 = fetch("https://jsonplaceholder.typicode.com/users").then(res =>
+             res.json()
+             );
+
+        Promise.all([promise1, promise2, promise3, promise4]).then(
+            (values) => console.log(values)
+            );
+    */
--- scriptTwo.js
+++ scriptTwo.js
@@ -1,8 +1,7 @@
 const posts = [
 
-    {title: "Post One", body: "This is post one",
-     title: "Post Two", body: "This is post two"   
-    }
+    {title: "Post One", body: "This is post one"},
+    { title: "Post Two", body: "This is post two" }  
 ];
 
 const getPosts = () => {
@@ -11,11 +10,20 @@ const getPosts = () => {
     setTimeout(() => {
 
     let output = "";
-    posts.ForEach((posts, index) => {
-        output += `<li>${posts.title}</li>`
+    posts.forEach((posts, index) => {
+        output += `<li>${posts.title}</li>`;
     });
     document.body.innerHTML = output;
     }, 1000);
 
 }
-getPosts();
+
+const createPost = (post, callback) => {
+
+    setTimeout(() => {
+        posts.push(post);
+        callback();
+    }, 2000);
+}
+
+createPost({title: "Post Three", body: "This is post three"}, getPosts);
